d21ddbddc098735e6b793580be9b719e1714fd9c
Ayush haldankar <haldankararpita50@gmail.com>
2025-09-18 22:04:32 +0530

    full site

---
+++ index.js
@@ -0,0 +1,17 @@
+var local = document.querySelector(".connect1")
+var local2 = document.querySelector(".connect2")
+local.addEventListener("click",function(){
+    window.location.href="https://x.com/ayushhaldankar1?t=oUBxkNGP4IsZO6mtw35KqQ&s=09"
+    })
+      
+        
+        
+local2.addEventListener("click",function(){
+        window.location.href="https://www.linkedin.com/in/ayush-haldankar-014613324/?lipi=urn%3Ali%3Apage%3Ad_flagship3_feed%3BQZmUQHyyQfWnmnajIlVvJA%3D%3D"
+        })
+
+
+// x = document.querySelector(".connect")
+$(".x").m(function(){
+    $("p").slidup();
+})
